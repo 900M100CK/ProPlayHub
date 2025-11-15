b50318e89127f649576cbbb054786a18132a8db0
thanh-thuy112403 <thuyttgcd220407@fpt.edu.vn>
2025-11-15 12:03:40 +0700

    update lại index.tsx
    .

--- Frontend/app/index.tsx
+++ Frontend/app/index.tsx
@@ -1,7 +1,11 @@
 import { Redirect } from 'expo-router';
 import React from 'react';
 
-
+/**
+ * Đây là tệp gốc của thư mục 'app'.
+ * Khi người dùng mở ứng dụng, tệp này sẽ được tải.
+ * Chúng ta dùng <Redirect> để tự động chuyển hướng họ đến trang đăng nhập.
+ */
 export default function AppIndex() {
-  return <Redirect href="./src/home/home.tsx"/>;
+  return <Redirect href="/src/home/home" />;
 }
